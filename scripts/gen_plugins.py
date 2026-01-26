29f819a17af0ce0d6b0c46934f29af799f7b4183
rhysrevans3 <rhys.r.evans3@gmail.com>
2026-01-26 08:16:19 +0000

    Deployed 1230ae1 with MkDocs version: 1.6.1

--- scripts/gen_plugins.py
+++ scripts/gen_plugins.py
@@ -11,30 +11,28 @@ ROOT = Path(__file__).parent.parent.parent
 PROJECT = ROOT / "extraction_methods"
 PLUGINS = PROJECT / "plugins"
 
-for group in PLUGINS.iterdir():
-    if group.is_dir():
-        for path in sorted(group.rglob("*.py")):
-            module_path = path.relative_to(ROOT).with_suffix("")
-            doc_path = path.relative_to(PLUGINS).with_suffix(".md")
-            full_doc_path = Path("reference", doc_path)
+for path in sorted(PLUGINS.rglob("*.py")):
+    module_path = path.relative_to(ROOT).with_suffix("")
+    doc_path = path.relative_to(PLUGINS).with_suffix(".md")
+    full_doc_path = Path("reference", doc_path)
 
-            parts = tuple(module_path.parts)
+    parts = tuple(module_path.parts)
 
-            # if parts[-1] == "__init__":
-            #     parts = parts[:-1]
-            #     doc_path = doc_path.with_name("index.md")
-            #     full_doc_path = full_doc_path.with_name("index.md")
-            if any(part.startswith(("_", ".")) for part in parts):
-                continue
+    # if parts[-1] == "__init__":
+    #     parts = parts[:-1]
+    #     doc_path = doc_path.with_name("index.md")
+    #     full_doc_path = full_doc_path.with_name("index.md")
+    if any(part.startswith(("_", ".")) for part in parts):
+        continue
 
-            nav_parts = [f"{mod_symbol} {part}" for part in parts]
-            nav[tuple(nav_parts)] = doc_path.as_posix()
+    nav_parts = [f"{mod_symbol} {part}" for part in parts]
+    nav[tuple(nav_parts)] = doc_path.as_posix()
 
-            with mkdocs_gen_files.open(doc_path, "w") as fd:
-                ident = ".".join(parts)
-                fd.write(f"::: {ident}")
+    with mkdocs_gen_files.open(doc_path, "w") as fd:
+        ident = ".".join(parts)
+        fd.write(f"::: {ident}")
 
-            mkdocs_gen_files.set_edit_path(full_doc_path, ".." / path.relative_to(ROOT))
+    mkdocs_gen_files.set_edit_path(full_doc_path, ".." / path.relative_to(ROOT))
 
 # with mkdocs_gen_files.open("reference/SUMMARY.md", "w") as nav_file:
 #     nav_file.writelines(nav.build_literate_nav())
